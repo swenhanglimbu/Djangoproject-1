195d0b9fd3c41ca744c28fcdb81a3787235ac060
Swen Limbu <swenlimbu@Swens-MacBook-Air.local>
2022-12-24 03:02:08 +0545

    added new models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.TextField(blank=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Information(models.Model):
+    address1 = models.CharField(max_length=300)
+    address2 = models.CharField(max_length=300)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    time = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.address1
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    logo = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    views = {}
+    views['service'] = Service.objects.all()
+    return render(request, 'index.html', views)
 
 def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    views = {}
+    views['infos'] = Information.objects.all()
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+            )
+        data.save()
+        views['mess'] = 'The message is Submitted'
+        return render(request, 'contact.html', views)
+
     return render(request, 'contact.html')
 
 def portfolio(request):
